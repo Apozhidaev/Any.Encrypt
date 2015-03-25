38217087cd20b56f268ae30aeee9363d81b014aa
alex <alexander.pozhidaev@ubs.com>
2015-03-25 12:46:11 +0300

    first commit

---
+++ Any.Encrypt/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Any.Encrypt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEncryptClick(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() ?? false)
+            {
+                using (var file = ofd.OpenFile())
+                using (var reader = new BinaryReader(file))
+                {
+                    var sh = new SecurityHelper(passText.Password);
+                    var content = reader.ReadBytes((int)file.Length);
+                    var eContent = sh.Encrypt(content);
+                    File.WriteAllBytes(String.Format("{0}.edata",ofd.FileName), eContent);
+                    MessageBox.Show("Ok");
+                }
+            }
+        }
+
+        private void OnDecryptClick(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() ?? false)
+            {
+                using (var file = ofd.OpenFile())
+                using (var reader = new BinaryReader(file))
+                {
+                    var sh = new SecurityHelper(passText.Password);
+                    var content = reader.ReadBytes((int)file.Length);
+                    var dContent = sh.Decrypt(content);
+                    File.WriteAllBytes(String.Format("{0}.ddata", ofd.FileName), dContent);
+                    MessageBox.Show("Ok");
+                }
+            }
+        }
+    }
+}
---
+++ Any.Encrypt/SecurityHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Any.Encrypt
+{
+    public class SecurityHelper
+    {
+        private readonly byte[] Key;
+
+        private readonly byte[] IV;
+
+        public SecurityHelper(string password)
+        {
+            const string saltStr = "1agg$wew";
+            const string ivStr = "ddfgsd%a";
+
+            var salt = new UTF8Encoding(false).GetBytes(saltStr);
+            var key = new Rfc2898DeriveBytes(password, salt);
+            var iv = new Rfc2898DeriveBytes(ivStr, salt);
+
+            Key = key.GetBytes(32);
+            IV = iv.GetBytes(16);
+        }
+
+        public byte[] Encrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            using (var algorithm = Aes.Create())
+            using (ICryptoTransform encryptor = algorithm.CreateEncryptor(key, iv))
+            {
+                return Crypt(data, encryptor);
+            }
+        }
+
+        public byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            using (var algorithm = Aes.Create())
+            using (ICryptoTransform decryptor = algorithm.CreateDecryptor(key, iv))
+            {
+                return Crypt(data, decryptor);
+            }
+        }
+
+        private byte[] Crypt(byte[] data, ICryptoTransform cryptor)
+        {
+            var m = new MemoryStream();
+            using (Stream c = new CryptoStream(m, cryptor, CryptoStreamMode.Write))
+            {
+                c.Write(data, 0, data.Length);
+            }
+            return m.ToArray();
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            return Encrypt(data, Key, IV);
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            return Decrypt(data, Key, IV);
+        }
+ 
+    }
+}
